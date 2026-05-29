fef35fb38599ed72069ba55cc6b65bfa42667e7e
neurothos42 <neurothos.dev@gmail.com>
2026-05-29 21:07:35 +0530

    Add DB_NAME to ENV configuration

--- server/src/lib/ENV.js
+++ server/src/lib/ENV.js
@@ -1,15 +1,16 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config({ quiet: true })
+dotenv.config({ quiet: true });
 
 export const ENV = {
-    PORT: process.env.PORT,
-    NODE_ENV: process.env.NODE_ENV,
-    DB_URL: process.env.DB_URL,
-    // SuperAdmin_Email: process.env.SuperAdmin_Email,
-    // Password: process.env.Password,
-    // CLOUD_NAME: process.env.CLOUD_NAME,
-    // CLOUD_API_KEY: process.env.CLOUD_API_KEY,
-    // CLOUD_API_SECRET: process.env.CLOUD_API_SECRET,
-    CLIENT_URL: process.env.CLIENT_URL,
-}
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  DB_URL: process.env.DB_URL,
+  // SuperAdmin_Email: process.env.SuperAdmin_Email,
+  // Password: process.env.Password,
+  // CLOUD_NAME: process.env.CLOUD_NAME,
+  // CLOUD_API_KEY: process.env.CLOUD_API_KEY,
+  // CLOUD_API_SECRET: process.env.CLOUD_API_SECRET,
+  CLIENT_URL: process.env.CLIENT_URL,
+  DB_NAME: process.env.DB_NAME,
+};
--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -1,15 +1,17 @@
-import mongoose from "mongoose"
-import { ENV } from "./ENV.js"
+import mongoose from "mongoose";
+import { ENV } from "./ENV.js";
 
 export const connectDB = async () => {
-    try {
-        if (!ENV.DB_URL) {
-            throw new Error('DB_URL is not defined in environment variables')
-        }
-        const connect = await mongoose.connect(ENV.DB_URL)
-        console.log("Mongodb is connected at", connect.connection.host)
-    } catch (error) {
-        console.error("Mongodb is not connected", error)
-        process.exit(1)
+  try {
+    if (!ENV.DB_URL) {
+      throw new Error("DB_URL is not defined in environment variables");
     }
-}
+    const connect = await mongoose.connect(ENV.DB_URL, {
+      dbName: ENV.DB_NAME,
+    });
+    console.log("Mongodb is connected at", connect.connection.host);
+  } catch (error) {
+    console.error("Mongodb is not connected", error);
+    process.exit(1);
+  }
+};
